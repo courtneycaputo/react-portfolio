b9ead3530519d5c20b26ad779c73aacf4e2154d3
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-31 15:40:01 -0700

    begin folder and file structure set up

---
+++ src/components/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import myPicture from '../../assets/mypicture.png';
+
+
+export default function Home() {
+  return (
+    <div>
+      <center><img src={myPicture} className="my-picture" alt="Courtney" /></center>
+      <h1>Courtney Caputo</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus.
+      </p>
+    </div>
+  );
+}
