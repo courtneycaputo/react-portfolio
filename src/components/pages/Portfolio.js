1755834d0110123f1fd6b0820b77ba7f84765a23
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-11-01 12:56:21 -0700

    start creating cards on portfolio page for projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import projects from '../../projects.json';
+import { Card } from 'react-bootstrap';
 
-export default function Portfolio() {
+function Portfolio(props) {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>
-        This is my portfolio
-      </p>
+    <div className="container">
+      
+      
+      <Card>
+      <img src={props.image} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      
+      <div className="card-body">
+        <a href={props.github} className="card-link">Card link</a>
+        <a href={props.deployedlink} className="card-link">Another link</a>
+      </div>
+    </Card>
+
+
     </div>
   );
 }
+
+
+function Projects () {  
+  return (
+  <>
+  <div className="container">
+  <h1>Portfolio</h1>
+  </div>
+
+    <div>
+      {projects.map((project) => (
+        <Portfolio 
+        name={project.name}
+        key={project.id}
+        image={project.image}
+        github={project.github}
+        deployedapp={project.deployedapp}
+        skills={project.skills}
+        description={project.description}
+        />
+      ))}
+    </div>
+  </>
+  );
+}
+
+export default Projects;
