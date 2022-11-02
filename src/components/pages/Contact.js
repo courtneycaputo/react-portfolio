379af32c22bd392ef052850cdd18134d9165c71a
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-11-02 08:00:49 -0700

    update styling of contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,79 +1,85 @@
 import React, { useState } from 'react';
 
-function Contact() {  
-    const [formState, setFormState] = useState({
-      name: "",
-      email:"",
-      message: ""
-    });
-    const [errorMessage, setErrorMessage] = useState("");
-    const {name, email, message } = formState;
+function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      if(!errorMessage) {
-        alert("Submit Form")
-        console.log("submit form", formState)
-      }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!errorMessage) {
+      alert("Submit Form")
+      console.log("submit form", formState)
     }
+  }
 
-    const validateEmail = (email) => {
+  const validateEmail = (email) => {
     const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-      return re.test(String(email).toLowerCase())
-    }
+    return re.test(String(email).toLowerCase())
+  }
 
-    const handleChange = (event) => { 
-      if(event.target.name === "email") {
-        const isValid = validateEmail(event.target.value);
-        if(!isValid){
-          setErrorMessage("Your email is invalid")
-        } else {
-          setErrorMessage("");
-        }
+  const handleChange = (event) => {
+    if (event.target.name === "email") {
+      const isValid = validateEmail(event.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid")
       } else {
-          if(!event.target.value.length) {
-            setErrorMessage(`${event.target.name} is required.`)
-          } else {
-            setErrorMessage("")
-          }
-          if(!errorMessage) {
-            setFormState({...formState, [event.target.name]: event.target.value});
-            console.log("Handle Form", formState);
-          }
+        setErrorMessage("");
       }
+    } else {
+      if (!event.target.value.length) {
+        setErrorMessage(`${event.target.name} is required.`)
+      } else {
+        setErrorMessage("")
+      }
+      if (!errorMessage) {
+        setFormState({ ...formState, [event.target.name]: event.target.value });
+        console.log("Handle Form", formState);
+      }
+    }
   }
 
   return (
-    <div className="container">
-      <h1>Contact Page</h1>
 
+    <section className="container text-color">
+      <h1>Contact Form</h1>
+      
       <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
+        <div className="form-spacing">
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
+          <input className="formcontrol" type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
-
-        <div>
+        
+        <div className="form-spacing">
           <label htmlFor="email">Email Address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
+          <input className="formcontrol" type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
-
-        <div>
+        
+        <div className="form-spacing">
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
+          <textarea className="formcontrol" name="message" defaultValue={message} onBlur={handleChange} rows="7" />
         </div>
-
+        
         {errorMessage && (
           <div>
-            <p>{errorMessage}</p>
+            <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button type="submit">Submit</button>
-      </form>
 
-    </div>
+        <div className="form-spacing">
+          <button type="submit">Send</button>
+        </div>
+      
+      </form>
+    
+    </section>
+    
   )
-  
+
 }
 
 export default Contact;
